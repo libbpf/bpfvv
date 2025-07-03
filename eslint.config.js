13cb88e7ce0cd25933d3c0f79085c76307ef4083
Ihor Solodrai <isolodrai@meta.com>
2025-07-03 15:01:04 -0700

    Add ESLint configuration
    ESLint [1] is a widely adopted static code analysis tool for
    JavaScript/TypeScript code.
    
    Add relevant dependencies and set up basic lint config.
    
    [1] https://eslint.org/
    [2] https://typescript-eslint.io/
    
    Signed-off-by: Ihor Solodrai <isolodrai@meta.com>

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        console: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      'no-undef': 'off',
+      'no-case-declarations': 'off',
+    },
+  },
+  {
+    files: ['cli/**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+  {
+    ignores: ['node_modules/**', 'dist/**', 'coverage/**'],
+  },
+];
