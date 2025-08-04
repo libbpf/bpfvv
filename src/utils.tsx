11610a11fd6ed015b027d781d2650c42b19aeea4
Jordan Rome <jordalgo@meta.com>
2025-08-04 10:24:46 -0400

    Add C Source Line View
    This adds a collapsible C source line view
    on the right, which contains all C files
    from the log and their C lines.
    This view is in sync with the main log lines
    view; meaning if a user clicks on a line
    in one, we'll hightlight the associated lines
    in the other.
    
    This also removes C lines from the main
    log view by default but a checkbox was added
    so users can add them back in as desired.
    
    Additional changes:
    - remove line numbers from the main log view
    - remove Go To Line input box
    - Add C Line to State Panel

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,3 +1,5 @@
+import { ParsedLine, ParsedLineType } from "./parser";
+
 export async function fetchLogFromUrl(url: string) {
   try {
     const response = await fetch(url);
@@ -22,7 +24,7 @@ export function getVisibleIdxRange(linesLen: number): {
   const formattedLogLines = document.getElementById("formatted-log-lines");
   const logContainer = document.getElementById("log-container");
   if (!formattedLogLines || !logContainer) {
-    return { min: 0, max: 0 };
+    return { min: 0, max: 0 }; // Throw here
   }
   const linesRect = formattedLogLines.getBoundingClientRect();
   const containerRect = logContainer.getBoundingClientRect();
@@ -39,13 +41,43 @@ export function getVisibleIdxRange(linesLen: number): {
   return { min, max };
 }
 
-export function scrollToLine(idx: number, linesLen: number) {
-  const logContainer = document.getElementById("log-container");
-  if (!logContainer) {
-    return;
-  }
+function scrollToLine(el: HTMLElement, idx: number, linesLen: number) {
   const { min, max } = getVisibleIdxRange(linesLen);
   const page = max - min + 1;
   const relativePosition = normalIdx(idx - page * 0.618, linesLen) / linesLen;
-  logContainer.scrollTop = relativePosition * logContainer.scrollHeight;
+  el.scrollTop = relativePosition * el.scrollHeight;
+}
+
+export function scrollToLogLine(idx: number, linesLen: number) {
+  const logContainer = document.getElementById("log-container");
+  if (!logContainer) {
+    throw new Error("Log line container is not in the DOM");
+  }
+  scrollToLine(logContainer, idx, linesLen);
+}
+
+export function scrollToCLine(idx: number, linesLen: number) {
+  const cSourceContainer = document.getElementById("c-source-container");
+  if (!cSourceContainer) {
+    // This won't exist if the container is collapsed
+    return;
+  }
+  scrollToLine(cSourceContainer, idx, linesLen);
+}
+
+export function siblingInsLine(
+  insLines: ParsedLine[],
+  idx: number,
+  delta: number,
+): number {
+  // if delta is 1 we are looking for the next instruction
+  // if delta is -1 we are looking for the previous instruction
+  const n = insLines.length;
+  for (let i = normalIdx(idx + delta, n); 0 <= i && i < n; i += delta) {
+    const line = insLines[i];
+    if (line.type === ParsedLineType.INSTRUCTION) {
+      return i;
+    }
+  }
+  return normalIdx(idx, n);
 }
